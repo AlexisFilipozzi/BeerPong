0c620df777feeead644db00fdd3c6e0a7db5cbf8
Alexis Filipozzi <alexis.filipozzi@student.ecp.fr>
2015-10-19 21:48:35 -0700

    add floor

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -3,13 +3,44 @@ using System.Collections;
 
 public class BallController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public float forceFactor;
+
+    private float m_startTime;
+    private Vector3 m_startPosition;
+    private Rigidbody m_rb;
+
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMouseDown()
+    {
+        m_startTime = Time.time;
+        m_startPosition = getPosition();
+        print("Test");
+
+        // set position of the ball
+
+    }
+
+    Vector3 getPosition()
+    {
+        Vector3 result = Input.mousePosition;
+        result.z = transform.position.z - Camera.main.transform.position.z;
+        result = Camera.main.ScreenToWorldPoint(result);
+        return result;
+    }
+
+    void OnMouseUp()
+    {
+        Vector3 endPosition = getPosition();
+
+        Vector3 force = endPosition - m_startPosition;
+        force.z = force.magnitude;
+
+        m_rb.isKinematic = false;
+        m_rb.AddForce(force * forceFactor);
+    }
+    
 }
